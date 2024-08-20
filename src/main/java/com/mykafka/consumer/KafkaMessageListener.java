d3f3de780db13f1cebac1e2434915c3c56b93ed1
skdod2453 <skdod2453@gmail.com>
2024-08-21 00:17:53 +0900

    multiple consumer

--- src/main/java/com/mykafka/consumer/KafkaMessageListener.java
+++ src/main/java/com/mykafka/consumer/KafkaMessageListener.java
@@ -13,6 +13,21 @@ public class KafkaMessageListener {
     // @KafkaListener(topics = "my-topic1", groupId = "my-group-n")
     @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String message) {
-        log.info("Consumer consume the message {} ", message);
+        log.info("Consumer1 consume the message {} ", message);
+    }
+
+    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+    public void consume2(String message) {
+        log.info("consumer2 consume the message {} ", message);
+    }
+
+    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+    public void consume3(String message) {
+        log.info("consumer3 consume the message {} ", message);
+    }
+
+    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+    public void consume4(String message) {
+        log.info("consumer4 consume the message {} ", message);
     }
 }
